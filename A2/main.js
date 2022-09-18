027ff83ef1b76cf3943a00a922cf0ab2517547f4
Triptiskillz <sharmatripti526@gmail.com>
2022-09-18 15:10:50 +0530

    task upload

---
+++ A2/main.js
@@ -0,0 +1,24 @@
+document.querySelector('#add').onclick = function () {
+    if (document.querySelector('#newtask input').value.length == 0) {
+        alert("Add Task")
+    }
+    else {
+        document.querySelector('#list').innerHTML += `
+    <div class="task">
+        <span id="taskname">
+            ${document.querySelector('#newtask input').value}
+        </span>
+        <button class="delete">
+            <i class="far fa-trash-alt"></i>
+        </button>
+    </div>
+`;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
